e815a7f739d4f93670faee78cac68f31b73af5ba
Grant <gcg4wg@virginia.edu>
2018-01-21 15:10:20 -0500

    get first two function working

---
+++ functions/index.js
@@ -0,0 +1,16 @@
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+const serviceAccount = require('./service_account.json');
+
+const createUser = require('./create_user');
+const requestPassword = require('./request_password');
+// const verifyPassword = require('./verify_password');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://yin-timer-2.firebaseio.com"
+});
+
+exports.createUser = functions.https.onRequest(createUser);
+exports.requestPassword = functions.https.onRequest(requestPassword);
+// exports.verifyPassword = functions.https.onRequest(verifyPassword);
